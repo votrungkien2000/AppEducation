cedc2ea09d0bc234ff2da21bcee12db6fc59a12b
LAPTOP-3P4KA4BV\ACER - PC <kiensua2000@gmail.com>
2025-04-15 16:34:42 +0700

    commit

--- src/layout/component/Header/Header.js
+++ src/layout/component/Header/Header.js
@@ -2,96 +2,96 @@ import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '@/assets/images';
 import config from '@/config';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCircleQuestion,
-  faCoins,
-  faEarthAsia,
-  faEllipsisVertical,
-  faGear,
-  faKeyboard,
-  faSignIn,
-  faSignOut,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+//   faCircleQuestion,
+//   faCoins,
+//   faEarthAsia,
+//   faEllipsisVertical,
+//   faGear,
+//   faKeyboard,
+//   faSignIn,
+//   faSignOut,
+//   faUser,
+// } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import Tippy from '@tippyjs/react';
+// import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 
-import Button from '@/component/Button/Button';
-import Menu from '@/component/Pooper/Menu';
-import { UploadIcon, MessageIcon, InboxIcon } from '@/component/Icon/Icon';
-import Image from '@/component/Image/Image';
-import Search from '../Search';
+// import Button from '@/component/Button/Button';
+// import Menu from '@/component/Pooper/Menu';
+// import { UploadIcon, MessageIcon, InboxIcon } from '@/component/Icon/Icon';
+// import Image from '@/component/Image/Image';
+// import Search from '../Search';
 
 function Header() {
   const cx = classNames.bind(styles);
-  const currentUser = true;
+  // const currentUser = true;
 
-  const MENU_ITEMS = [
-    {
-      icon: <FontAwesomeIcon icon={faEarthAsia} />,
-      title: 'English',
-      children: {
-        title: 'language',
-        data: [
-          {
-            type: 'language',
-            code: 'en',
-            title: 'English',
-          },
-          {
-            type: 'language',
-            code: 'vi',
-            title: 'Tiếng Việt',
-          },
-        ],
-      },
-    },
-    {
-      icon: <FontAwesomeIcon icon={faCircleQuestion} />,
-      title: 'Feedback and help',
-      to: 'http://localhost:3000/feedback',
-    },
-    {
-      icon: <FontAwesomeIcon icon={faKeyboard} />,
-      title: 'keyboard shortcuts',
-    },
-  ];
+  // const MENU_ITEMS = [
+  //   {
+  //     icon: <FontAwesomeIcon icon={faEarthAsia} />,
+  //     title: 'English',
+  //     children: {
+  //       title: 'language',
+  //       data: [
+  //         {
+  //           type: 'language',
+  //           code: 'en',
+  //           title: 'English',
+  //         },
+  //         {
+  //           type: 'language',
+  //           code: 'vi',
+  //           title: 'Tiếng Việt',
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+  //     title: 'Feedback and help',
+  //     to: 'http://localhost:3000/feedback',
+  //   },
+  //   {
+  //     icon: <FontAwesomeIcon icon={faKeyboard} />,
+  //     title: 'keyboard shortcuts',
+  //   },
+  // ];
 
-  const userMenu = [
-    {
-      icon: <FontAwesomeIcon icon={faUser} />,
-      title: 'View profile',
-      to: '/@hoaa',
-    },
-    {
-      icon: <FontAwesomeIcon icon={faCoins} />,
-      title: 'Get coins',
-      to: '/coins',
-    },
-    {
-      icon: <FontAwesomeIcon icon={faGear} />,
-      title: 'Setting',
-      to: '/setting',
-    },
-    ...MENU_ITEMS,
-    {
-      icon: <FontAwesomeIcon icon={faSignOut} />,
-      title: 'Log out',
-      to: '/logout',
-      separate: true,
-    },
-  ];
+  // const userMenu = [
+  //   {
+  //     icon: <FontAwesomeIcon icon={faUser} />,
+  //     title: 'View profile',
+  //     to: '/@hoaa',
+  //   },
+  //   {
+  //     icon: <FontAwesomeIcon icon={faCoins} />,
+  //     title: 'Get coins',
+  //     to: '/coins',
+  //   },
+  //   {
+  //     icon: <FontAwesomeIcon icon={faGear} />,
+  //     title: 'Setting',
+  //     to: '/setting',
+  //   },
+  //   ...MENU_ITEMS,
+  //   {
+  //     icon: <FontAwesomeIcon icon={faSignOut} />,
+  //     title: 'Log out',
+  //     to: '/logout',
+  //     separate: true,
+  //   },
+  // ];
 
   //handle Logic
-  const handleMenuChange = (nemuItem) => {
-    switch (nemuItem.type) {
-      case 'language':
-        break;
-      default:
-    }
-  };
+  // const handleMenuChange = (nemuItem) => {
+  //   switch (nemuItem.type) {
+  //     case 'language':
+  //       break;
+  //     default:
+  //   }
+  // };
 
   return (
     <header className={cx('wrapper')}>
